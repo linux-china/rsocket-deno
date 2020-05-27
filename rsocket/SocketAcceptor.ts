38dfd802bcab41eb00dc0fc3993645b19d124fe8
linux_china <libing.chen@gmail.com>
2020-05-26 17:51:09 -0700

    SocketAcceptor

---
+++ rsocket/SocketAcceptor.ts
@@ -0,0 +1,56 @@
+import {RSocket, AbstractRSocket} from "./RSocket.ts";
+import {ConnectionSetupPayload, Payload} from "./Payload.ts";
+import {Publisher} from "../reactivestreams/mod.ts";
+
+export interface SocketAcceptor {
+    accept(setup: ConnectionSetupPayload, sendingSocket: RSocket): RSocket
+}
+
+export function forRequestResponse(handler: (payload: Payload) => Promise<Payload>): SocketAcceptor {
+    return {
+        accept(setup: ConnectionSetupPayload, sendingSocket: RSocket): AbstractRSocket {
+            return new class extends AbstractRSocket {
+                requestResponse(payload: Payload): Promise<Payload> {
+                    return handler(payload);
+                }
+            }();
+        }
+    }
+}
+
+
+export function forFireAndForget(handler: (payload: Payload) => Promise<void>): SocketAcceptor {
+    return {
+        accept(setup: ConnectionSetupPayload, sendingSocket: RSocket): AbstractRSocket {
+            return new class extends AbstractRSocket {
+                fireAndForget(payload: Payload): Promise<void> {
+                    return handler(payload);
+                }
+            }();
+        }
+    }
+}
+
+export function forRequestStream(handler: (payload: Payload) => Publisher<Payload>): SocketAcceptor {
+    return {
+        accept(setup: ConnectionSetupPayload, sendingSocket: RSocket): AbstractRSocket {
+            return new class extends AbstractRSocket {
+                requestStream(payload: Payload): Publisher<Payload> {
+                    return handler(payload);
+                }
+            }();
+        }
+    }
+}
+
+export function forRequestChannel(handler: (payloads: Publisher<Payload>) => Publisher<Payload>): SocketAcceptor {
+    return {
+        accept(setup: ConnectionSetupPayload, sendingSocket: RSocket): AbstractRSocket {
+            return new class extends AbstractRSocket {
+                requestChannel(payloads: Publisher<Payload>): Publisher<Payload> {
+                    return handler(payloads);
+                }
+            }();
+        }
+    }
+}
