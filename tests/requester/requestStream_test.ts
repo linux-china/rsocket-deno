b9cace71c07eb302fd9f2fb3e6d3c93494717803
linux_china <libing.chen@gmail.com>
2020-05-26 21:40:31 -0700

    polishing

--- tests/requester/requestStream_test.ts
+++ tests/requester/requestStream_test.ts
@@ -1,10 +1,10 @@
 import {RSocketConnector, Payload} from "../../mod.ts"
-import {publisherToAsyncIterator} from "../../reactivestreams/mod.ts";
+import {Publisher, publisherToAsyncIterator} from "../../reactivestreams/mod.ts";
 
 const rsocket = await RSocketConnector.create().connect("ws://127.0.0.1:42252");
 
 Deno.test("RSocket RequestStream", async () => {
-    const publisher = rsocket.requestStream(Payload.fromText("Word Up", "metadata"));
+    const publisher: Publisher<Payload> = rsocket.requestStream(Payload.fromText("Word Up", "metadata"));
     // async iterator style: await for ... of
     for await (const data of publisherToAsyncIterator(publisher)) {
         console.log(data.getDataUtf8());
