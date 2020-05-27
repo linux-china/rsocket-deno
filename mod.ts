03510659816e8d5727b7720b7d7b01f94e99539c
linux_china <libing.chen@gmail.com>
2020-05-26 17:53:33 -0700

    RSocket Den mod.ts

---
+++ mod.ts
@@ -0,0 +1,78 @@
+export {Payload, ConnectionSetupPayload} from "./rsocket/Payload.ts";
+export {RSocketConnector} from "./rsocket/RSocketConnector.ts";
+export {RSocketServer} from "./rsocket/RSocketServer.ts";
+export {
+    SocketAcceptor,
+    forRequestResponse,
+    forFireAndForget,
+    forRequestStream,
+    forRequestChannel
+} from "./rsocket/SocketAcceptor.ts";
+
+export {RSocket} from "./rsocket/RSocket.ts";
+export {
+    CompositeMetadata,
+    MetadataEntry,
+    WellKnownMimeTypeEntry,
+    ExplicitMimeTypeEntry,
+    TaggingMetadata,
+    RoutingMetadata,
+    AuthMetadata,
+    MessageMimeTypeMetadata,
+    MessageAcceptMimeTypesMetadata,
+    ZipkinTracingMetadata
+} from "./rsocket/metadata/CompositeMetadata.ts";
+
+export {
+    WellKnownMimeType,
+    addWellKnownType,
+    UNPARSEABLE_MIME_TYPE,
+    UNKNOWN_RESERVED_MIME_TYPE,
+    APPLICATION_AVRO,
+    APPLICATION_CBOR,
+    APPLICATION_GRAPHQL,
+    APPLICATION_GZIP,
+    APPLICATION_JAVASCRIPT,
+    APPLICATION_JSON,
+    APPLICATION_OCTET_STREAM,
+    APPLICATION_PDF,
+    APPLICATION_THRIFT,
+    APPLICATION_PROTOBUF,
+    APPLICATION_XML,
+    APPLICATION_ZIP,
+    AUDIO_AAC,
+    AUDIO_MP3,
+    AUDIO_MP4,
+    AUDIO_MPEG3,
+    AUDIO_MPEG,
+    AUDIO_OGG,
+    AUDIO_OPUS,
+    AUDIO_VORBIS,
+    IMAGE_BMP,
+    IMAGE_GIG,
+    IMAGE_HEIC_SEQUENCE,
+    IMAGE_HEIC,
+    IMAGE_HEIF_SEQUENCE,
+    IMAGE_HEIF,
+    IMAGE_JPEG,
+    IMAGE_PNG,
+    IMAGE_TIFF,
+    MULTIPART_MIXED,
+    TEXT_CSS,
+    TEXT_CSV,
+    TEXT_HTML,
+    TEXT_PLAIN,
+    TEXT_XML,
+    VIDEO_H264,
+    VIDEO_H265,
+    VIDEO_VP8,
+    APPLICATION_HESSIAN,
+    APPLICATION_JAVA_OBJECT,
+    APPLICATION_CLOUDEVENTS_JSON,
+    MESSAGE_RSOCKET_MIMETYPE,
+    MESSAGE_RSOCKET_ACCEPT_MIMETYPES,
+    MESSAGE_RSOCKET_AUTHENTICATION,
+    MESSAGE_RSOCKET_TRACING_ZIPKIN,
+    MESSAGE_RSOCKET_ROUTING,
+    MESSAGE_RSOCKET_COMPOSITE_METADATA,
+} from "./rsocket/metadata/WellKnownMimeType.ts";
