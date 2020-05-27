b2f9e375ca4bca5ea72e3cc6f9415034b77f6435
linux_china <libing.chen@gmail.com>
2020-05-26 17:55:03 -0700

    deps for RSocket Deno module

---
+++ deps.ts
@@ -0,0 +1,8 @@
+export {encode, decode} from "https://deno.land/std@v0.53.0/encoding/utf8.ts"
+export {
+    WebSocket,
+    connectWebSocket,
+    isWebSocketCloseEvent,
+    isWebSocketPingEvent,
+    isWebSocketPongEvent
+} from "https://deno.land/std@v0.53.0/ws/mod.ts"
