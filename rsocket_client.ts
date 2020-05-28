6287e19580e347ca290c90421c8653e93cf483bf
linux_china <libing.chen@gmail.com>
2020-05-27 18:12:19 -0700

    polishing

--- rsocket_client.ts
+++ rsocket_client.ts
@@ -5,5 +5,5 @@ import {
 
 const rsocket = await RSocketConnector.create().connect("tcp://127.0.0.1:42252");
 
-const result: Payload = await rsocket.requestResponse(Payload.fromText("Hello, I'm requester!", "metadata"));
+const result = await rsocket.requestResponse(Payload.fromText("Hello, I'm requester!", ""));
 console.log(result.getDataUtf8());
