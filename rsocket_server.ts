3cdc956c22ba8284ac33d5e7dd2b29fbbd19d1b9
linux_china <libing.chen@gmail.com>
2020-05-27 18:01:51 -0700

    polishing

--- rsocket_server.ts
+++ rsocket_server.ts
@@ -2,7 +2,7 @@ import {Payload, RSocketServer, forRequestResponse} from "https://deno.land/x/rs
 
 await RSocketServer.create(forRequestResponse(async (payload: Payload): Promise<Payload> => {
     console.log(`Received: ${payload.getDataUtf8()} `)
-    return Payload.fromText("Hello, this is Deno Server!", "metadata");
+    return Payload.fromText("Hello, this is Deno Server!", "");
 })).bind("tcp://0.0.0.0:42252");
 
 console.log("RSocket Server started on 0.0.0.0:42252")
