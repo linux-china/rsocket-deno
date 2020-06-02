3e8364d0486bd68e3dd2589cc521b0c11ec3fccc
linux_china <libing.chen@gmail.com>
2020-06-02 10:32:17 -0700

    update to std 0.55.0

--- deps.ts
+++ deps.ts
@@ -1,8 +1,8 @@
-export {encode, decode} from "https://deno.land/std@v0.53.0/encoding/utf8.ts"
+export {encode, decode} from "https://deno.land/std@v0.55.0/encoding/utf8.ts"
 export {
     WebSocket,
     connectWebSocket,
     isWebSocketCloseEvent,
     isWebSocketPingEvent,
     isWebSocketPongEvent
-} from "https://deno.land/std@v0.53.0/ws/mod.ts"
+} from "https://deno.land/std@v0.55.0/ws/mod.ts"
