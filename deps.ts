2a4e01863279d5f5a03685f3724fb2b91d57a009
linux_china <libing.chen@gmail.com>
2020-06-09 20:45:14 -0700

    add websocket

--- deps.ts
+++ deps.ts
@@ -1,8 +1,11 @@
 export {encode, decode} from "https://deno.land/std@v0.56.0/encoding/utf8.ts"
 export {
     WebSocket,
+    acceptWebSocket,
     connectWebSocket,
     isWebSocketCloseEvent,
     isWebSocketPingEvent,
     isWebSocketPongEvent
 } from "https://deno.land/std@v0.56.0/ws/mod.ts"
+
+export {Server} from "https://deno.land/std@v0.56.0/http/server.ts";
