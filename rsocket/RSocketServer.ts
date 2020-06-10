d58b2f9f1a89bafa9594e96b4cbc0b1e9803cc65
linux_china <libing.chen@gmail.com>
2020-06-09 20:40:15 -0700

    add websocket support

--- rsocket/RSocketServer.ts
+++ rsocket/RSocketServer.ts
@@ -1,6 +1,7 @@
 import Closer = Deno.Closer;
-import {RSocketResponder} from "./core/RSocketResponder.ts";
+import {RSocketResponder, RSocketWebSocketResponder} from "./core/RSocketResponder.ts";
 import {SocketAcceptor} from "./SocketAcceptor.ts";
+import {serve} from "https://deno.land/std/http/server.ts";
 
 export class RSocketServer {
     private readonly _acceptor: SocketAcceptor
@@ -16,11 +17,17 @@ export class RSocketServer {
     public bind(url: string): Closer {
         let schema = url.substring(0, url.indexOf(":"))
         let urlObj = new URL(url.replace(schema + "://", "http://"))
-        //todo WebSocket support
-        const listener = Deno.listen({port: parseInt(urlObj.port)});
-        let rSocketResponder = new RSocketResponder(urlObj, listener, this._acceptor);
-        rSocketResponder.accept().then();
-        return rSocketResponder;
+        if (schema === "ws") {
+            let serve1 = serve(`:${urlObj.port}`);
+            let rSocketResponder = new RSocketWebSocketResponder(urlObj, serve1, this._acceptor);
+            rSocketResponder.accept().then();
+            return rSocketResponder;
+        } else {
+            const listener = Deno.listen({port: parseInt(urlObj.port)});
+            let rSocketResponder = new RSocketResponder(urlObj, listener, this._acceptor);
+            rSocketResponder.accept().then();
+            return rSocketResponder;
+        }
     }
 
 }
