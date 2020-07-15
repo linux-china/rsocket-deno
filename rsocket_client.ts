74496da7e8bb78e365905883d0b4916103efaeea
linux_china <libing.chen@gmail.com>
2020-07-15 01:29:16 -0700

    polishing

--- rsocket_client.ts
+++ rsocket_client.ts
@@ -5,5 +5,5 @@ import {
 
 const rsocket = await RSocketConnector.create().connect("tcp://127.0.0.1:42252");
 
-const result = await rsocket.requestResponse(Payload.fromText("Hello, I'm requester!", ""));
+const result = await rsocket.requestResponse(Payload.fromText("Ping", ""));
 console.log(result.getDataUtf8());
